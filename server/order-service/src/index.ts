947aebd1048c5534b22bd7107376ce8ee21632ac
lehaiduy2003 <lehaiduy2003@gmail.com>
2024-12-07 15:45:26 +0700

    mirror fix

--- server/order-service/src/index.ts
+++ server/order-service/src/index.ts
@@ -2,9 +2,16 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import * as swaggerDocument from "../swagger.json";
 import createOrderRoute from "./routes/orderRoute";
-
+import cors from "cors";
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:8080"],
+    credentials: true,
+  })
+);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1", createOrderRoute().getRouter());
